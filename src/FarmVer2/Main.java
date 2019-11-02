3573ff47e0ed914d0cc6cd568db060fe0d90d4e0
artemitmo <tema_l@mail.ru>
2019-11-02 15:16:44 +0300

    С исправлениями

--- src/FarmVer2/Main.java
+++ src/FarmVer2/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         FarmDay fd1 = new FarmDay();
         fd1.oneDay();
-        System.out.println("Победил фермер!!!");
     }
 }
